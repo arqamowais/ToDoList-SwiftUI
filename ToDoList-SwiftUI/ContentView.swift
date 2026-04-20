27e215021a22fd626d8352c38fe28e9e8f1e3a41
Arqam Owais <muhammadarqam912@gmail.com>
2026-04-21 01:44:17 +0500

    project setup

---
+++ ToDoList-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoList-SwiftUI
+//
+//  Created by Arqam Owais on 20/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList-SwiftUI/ToDoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIApp.swift
+//  ToDoList-SwiftUI
+//
+//  Created by Arqam Owais on 20/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
