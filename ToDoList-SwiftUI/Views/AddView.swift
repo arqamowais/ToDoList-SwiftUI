9ae6c529504dfd42df49765c7c9dc44deb899f9f
Arqam Owais <muhammadarqam912@gmail.com>
2026-04-23 03:15:19 +0500

    save item logic

--- ToDoList-SwiftUI/Views/AddView.swift
+++ ToDoList-SwiftUI/Views/AddView.swift
@@ -8,8 +8,13 @@
 import SwiftUI
 
 struct AddView: View {
+
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     
     @State private var textFieldText: String = ""
+    @State private var alertTitle: String = ""
+    @State private var alertIsVisible: Bool = false
     
     var body: some View {
         ScrollView {
@@ -20,9 +25,8 @@ struct AddView: View {
                     .background(Color(.lightGray))
                     .cornerRadius(10)
                 
-                Button(action: {
-                    
-                }, label: {
+                Button(action: saveButtonPressed,
+                       label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
                         .frame(height: 55)
@@ -35,9 +39,34 @@ struct AddView: View {
             
         }
         .navigationBarTitle("Add an Item")
+        .alert(isPresented: $alertIsVisible) {
+            getAlert()
+        }
+    }
+    
+    private func saveButtonPressed() {
+        if textIsAppropriateLength() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        } else {
+            alertTitle = "Your new item must be at least 3 characters long."
+            alertIsVisible.toggle()
+        }
+    }
+    
+    private func textIsAppropriateLength() -> Bool {
+        textFieldText.count > 3
+    }
+    
+    private func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
 #Preview {
-    AddView()
+    NavigationView {
+        AddView()
+    }
+    .environmentObject(ListViewModel())
+    
 }
